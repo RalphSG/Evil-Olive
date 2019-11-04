4f8440eb9a2f11de9b35fb18166ac9949c61ce9f
RalphSG <43961722+RalphSG@users.noreply.github.com>
2019-11-04 02:34:50 +0200

    Updated UVMap player + player controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotSpeed;
+    Rigidbody rb;
+    Animator anim;
+    BoxCollider colSize;
+
+    private Vector3 moveDirection = Vector3.zero;
+    public float maxTurnSpeed = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        colSize = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        var z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(x, 0, -z);
+    }
+}
