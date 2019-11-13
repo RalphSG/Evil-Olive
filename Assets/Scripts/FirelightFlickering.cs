dac5679317a62f7bb2695baf7ee6a7f56675d4ad
RalphSG <43961722+RalphSG@users.noreply.github.com>
2019-11-13 23:35:01 +0200

    Added Candle and firelight flickering + dungeon model for menu

---
+++ Assets/Scripts/FirelightFlickering.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirelightFlickering : MonoBehaviour
+{
+    public float minFlickerIntensity = 1.5f;
+    public float maxFlickerIntensity = 2.5f;
+    public float flickerSpeed = 0.035f;
+ 
+    private int randomizer = 0;
+
+    private Light light;
+
+    public void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine("lightFlickering");
+    }
+
+    IEnumerator lightFlickering() {
+        while (true)
+        {
+            if (randomizer == 0)
+            {
+                light.intensity = (Random.Range(minFlickerIntensity, maxFlickerIntensity));
+
+            }
+            else light.intensity = (Random.Range(minFlickerIntensity, maxFlickerIntensity));
+
+            randomizer = Random.Range(0, 1);
+            yield return new WaitForSeconds(flickerSpeed);
+        }
+    }
+}
