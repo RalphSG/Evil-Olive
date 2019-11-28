251938c612af4ca73267a4f6371ceacec8bbebbf
RalphSG <43961722+RalphSG@users.noreply.github.com>
2019-11-28 15:37:49 +0200

    Added angle display + normal to the mirror

---
+++ Assets/Scripts/AngleNumber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleNumber : MonoBehaviour
+{
+    private TextMesh angleText;
+    private int angleNumber;
+
+    GameObject reflexion;
+    Reflexion reflexionChild;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.localEulerAngles = new Vector3(0, 180, 0);
+        angleText = gameObject.GetComponent<TextMesh>();
+
+        reflexion = GameObject.FindGameObjectWithTag("Reflexion");
+        reflexionChild = reflexion.gameObject.GetComponent<Reflexion>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angleNumber = reflexionChild.angleInci;
+        angleText.text = angleNumber.ToString();
+    }
+}
---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.LookAt(Camera.main.transform);
+    }
+}
