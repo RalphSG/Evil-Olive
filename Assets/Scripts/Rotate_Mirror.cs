59d7828aa1a2514175ca73f52e9dde7c8ee2ccf1
slepikovskii <43961677+slepikovskii@users.noreply.github.com>
2019-12-04 00:41:33 +0200

    Rotating mirror script

---
+++ Assets/Scripts/Rotate_Mirror.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Mirror : MonoBehaviour
+{
+    public float speed = 3f;
+    public GameObject rmirror;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Rotate(0, 0, speed);
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Rotate(0, 0, speed);
+            }
+        }
+    }
+}
