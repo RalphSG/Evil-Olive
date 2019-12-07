6b430c29ec6e3a7088ce23e7c8ba72a935951490
RalphSG <43961722+RalphSG@users.noreply.github.com>
2019-12-07 18:10:27 +0200

    Added Credits scene

---
+++ Assets/Scripts/CreditsTextAndLine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsTextAndLine : MonoBehaviour
+{
+    public ParticleSystem puffPS;
+    public LineRenderer lineRend;
+
+    float colorPourcent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puffPS = GameObject.FindGameObjectWithTag("Puff").GetComponent<ParticleSystem>();
+        lineRend = GameObject.FindGameObjectWithTag("LineMR").GetComponent<LineRenderer>();
+        StartCoroutine("puff");
+    }
+
+    IEnumerator puff()
+    {
+        yield return new WaitForSeconds(1.95f);
+        puffPS.Play();
+        for (float i = 100; i > 0; i--)
+        {
+            Debug.Log(i);
+            colorPourcent = i / 100;
+            Debug.Log(colorPourcent);
+            lineRend.endColor = new Color(0, 0.8666667f, 1, colorPourcent);
+            colorPourcent = colorPourcent / 2;
+            lineRend.startColor = new Color(0, 0.8666667f, 1, colorPourcent);
+            yield return new WaitForSeconds(0.00005f);
+
+        }
+    }
+}
